64efdcce082a539a45e2e7884caac7249b00982e
Wouter Couvaras <wouter@hootoriginals.com>
2023-11-01 11:51:42 +0200

    getting ready for deploy

--- config/sources.config.ts
+++ config/sources.config.ts
@@ -1,10 +1,19 @@
 export const sources = {
-  "my-xform.vercel.app": {
+  "cdn.xform.media": {
     "name": "xform-media",
     "accessKeyId": "ACCESS_KEY_ID",
     "secretAccessKey": "SECRET_ACCESS_KEY",
     "bucket": "BUCKET",
     "region": "REGION",
+    "max-age": "31536000",
+    "s-maxage": "2592000",
+  },
+  "chaircrazy.xform.media": {
+    "name": "chaircrazy",
+    "accessKeyId": "CC_ACCESS_KEY_ID",
+    "secretAccessKey": "CC_SECRET_ACCESS_KEY",
+    "bucket": "CC_BUCKET",
+    "region": "CC_REGION",
     "max-age": "31536000",
     "s-maxage": "2592000",
   }
